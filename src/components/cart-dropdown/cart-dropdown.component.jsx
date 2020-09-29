ff0ad83b12d72e075fcce6155adb6f4b2c324912
walter708 <uchewalter8@gmail.com>
2020-09-29 15:16:37 -0400

    updating our application with reselect ,replased cart selector with memoized selectors

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -5,7 +5,10 @@ import CustomButton from '../custom-button/custom-button.component';
 import './cart-dropdown.style.scss';
 
 import {connect} from 'react-redux';
-import CartItem from '../cart-item/cart-item.component'
+
+import CartItem from '../cart-item/cart-item.component';
+
+import { selectCartItems } from '../../redux/cart/cart.selectors';
 
 const CartDropdown = ({cartItems}) => (
     <div className = 'cart-dropdown'>
@@ -18,7 +21,7 @@ const CartDropdown = ({cartItems}) => (
     </div>
 )
 
-const mapStateToProps = ({cart: {cartItems}})=>({
-     cartItems
+const mapStateToProps = (state)=>({
+    cartItems :selectCartItems(state)
 })
 export default connect(mapStateToProps)(CartDropdown);
--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,25 +1,34 @@
 import React from 'react';
 
+import { selectCartItemCount } from '../../redux/cart/cart.selectors';
+
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 
 import './cart-icon.style.scss';
 
 import {connect} from 'react-redux';
 
-import {toggleCartHidden} from '../../redux/cart/cart.action'
 
 
-const CartIcon = ({toggleCartHidden}) => (
+import {toggleCartHidden} from '../../redux/cart/cart.action';
+
+
+const CartIcon = ({toggleCartHidden , itemCount}) => (
     <div className = 'cart-icon' onClick= {toggleCartHidden}>
     <ShoppingIcon className = 'shopping-icon' />
-    <span className = 'item-count'> 0 </span> 
+    <span className = 'item-count'> {itemCount} </span> 
         
      </div>
 )
 const mapDispatchToProps =  dispatch => ({
     toggleCartHidden:()=>dispatch(toggleCartHidden())
 })
+
+const mapStateToProps =  (state) => ({
+    itemCount:  selectCartItemCount(state)
+})
+
 export default connect (
-    null ,
+    mapStateToProps,
     mapDispatchToProps)
     (CartIcon)
